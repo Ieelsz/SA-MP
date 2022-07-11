9bab316f2c517045e283b19bdce9973b4afb36c9
Daniel Oliveira <99140225+Ieelsz@users.noreply.github.com>
2022-07-11 18:25:57 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+//Cabeçalho Web Site
+require 'main.header.html';
+?>
+<main>
+    <section>
+    <hr class="barra">
+        <h1 class="h1-home">Home</h1>
+
+
+    </section>
+</main>
+<?php
+//RodaPé Web Site
+require 'main.footer.html';
+?>
